b4c916862f5ca22b609de51be98cc6bbcdd9cdf4
elenakarabeteva <e.karabeteva6@gmail.com>
2023-03-15 00:54:06 +0200

    made changes to logic

--- Week03/ForHome/Task01.hs
+++ Week03/ForHome/Task01.hs
@@ -1,22 +1,17 @@
 main :: IO()
 main = do
+    --print $ removeFistOccurrence 10230 5 
     print $ removeFistOccurrence 110 1 -- == 10
-    print $ removeFistOccurrence 15365 5 -- == 1536
-    -- print $ removeFistOccurrence 15360 0 == 1536
-    -- print $ removeFistOccurrence 15300 0 == 1530
-    -- print $ removeFistOccurrence 15365 1 == 5365
-    -- print $ removeFistOccurrence 35365 3 == 3565
-    -- print $ removeFistOccurrence 1212 1 == 122
-    -- print $ removeFistOccurrence 1212 2 == 121
-    -- print $ removeFistOccurrence (removeFistOccurrence 1212 1) 1 == 22
+    print $ removeFistOccurrence 15365 5 == 1536
+    print $ removeFistOccurrence 15360 0 == 1536
+    print $ removeFistOccurrence 15300 0 -- == 1530
+    print $ removeFistOccurrence 15365 1 == 5365
+    print $ removeFistOccurrence 35365 3 == 3565
+    print $ removeFistOccurrence 1212 1 == 122
+    print $ removeFistOccurrence 1212 2 == 121
+    print $ removeFistOccurrence (removeFistOccurrence 1212 1) 1 == 22
 
-countOccurrence :: Int -> Int -> Int
-countOccurrence number digit = helper number 0
- where
-    helper 0 count = count
-    helper number count
-     | mod number 10 == digit = helper (div number 10) (count + 1)
-     | otherwise = helper (div number 10) count
+--to do: count zeros at the end
 
 reversed :: Int -> Int
 reversed number = helper number 0
@@ -28,10 +23,7 @@ reversed number = helper number 0
 removeFistOccurrence :: Int -> Int -> Int
 removeFistOccurrence number digit = helper number 0 0
  where
-    helper 0 result occurrence = reversed result
-    helper number result occurrence
-     | mod number 10 == digit && occurrence == count = helper (div number 10) result occurrence
-     | mod number 10 == digit && occurrence /= count = helper (div number 10) (result * 10 + (mod number 10)) (occurrence + 1)
-     | otherwise = helper (div number 10) (result * 10 + (mod number 10)) occurrence
-     where
-        count = (countOccurrence number digit) - 1
+    helper 0 result count = reversed result
+    helper number result count
+     | mod number 10 == digit && count == 0 = helper (div number 10) result (count + 1)
+     | otherwise = helper (div number 10) (result * 10 + (mod number 10)) count
