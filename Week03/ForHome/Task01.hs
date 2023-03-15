96ee182d1ad9d1da9bf41b46476ee70ad3de18eb
elenakarabeteva <e.karabeteva6@gmail.com>
2023-03-15 11:57:20 +0200

    added new method to algorithm

--- Week03/ForHome/Task01.hs
+++ Week03/ForHome/Task01.hs
@@ -1,17 +1,18 @@
 main :: IO()
 main = do
-    --print $ removeFistOccurrence 10230 5 
-    print $ removeFistOccurrence 110 1 -- == 10
+    print $ removeFistOccurrence 110 1 == 10
     print $ removeFistOccurrence 15365 5 == 1536
     print $ removeFistOccurrence 15360 0 == 1536
-    print $ removeFistOccurrence 15300 0 -- == 1530
+    print $ removeFistOccurrence 15300 0 == 1530
     print $ removeFistOccurrence 15365 1 == 5365
     print $ removeFistOccurrence 35365 3 == 3565
     print $ removeFistOccurrence 1212 1 == 122
     print $ removeFistOccurrence 1212 2 == 121
     print $ removeFistOccurrence (removeFistOccurrence 1212 1) 1 == 22
 
---to do: count zeros at the end
+countDigits :: Int -> Int
+countDigits 0 = 0
+countDigits number = 1 + countDigits (div number 10)
 
 reversed :: Int -> Int
 reversed number = helper number 0
@@ -21,9 +22,9 @@ reversed number = helper number 0
     helper leftover result = helper (div leftover 10) (result * 10 + mod leftover 10)
 
 removeFistOccurrence :: Int -> Int -> Int
-removeFistOccurrence number digit = helper number 0 0
+removeFistOccurrence number digit = helper number 0 0 (countDigits number)
  where
-    helper 0 result count = reversed result
-    helper number result count
-     | mod number 10 == digit && count == 0 = helper (div number 10) result (count + 1)
-     | otherwise = helper (div number 10) (result * 10 + (mod number 10)) count
+    helper 0 result count digitCount = (reversed result) * 10^(digitCount - countDigits(reversed result)) -- adding zeros at the end
+    helper number result count digitCount
+     | mod number 10 == digit && count == 0 = helper (div number 10) result (count + 1) (digitCount - 1)
+     | otherwise = helper (div number 10) (result * 10 + (mod number 10)) count digitCount
---
+++ Week03/ForHome/Task02.hs
@@ -0,0 +1,40 @@
+main :: IO()
+main = do
+    print $ countDigits 123450
+    print $ findMax 123450
+    -- print $ sortN 1714 == 7411
+    -- print $ sortN 123450 == 543210
+    -- print $ sortN 123405 == 543210
+    -- print $ sortN 123045 == 543210
+    -- print $ sortN 120345 == 543210
+    -- print $ sortN 102345 == 543210
+    -- print $ sortN 8910 == 9810
+    -- print $ sortN 321 == 321
+    -- print $ sortN 29210 == 92210
+    -- print $ sortN 1230 == 3210
+    -- print $ sortN 55345 == 55543
+    -- print $ sortN 14752 == 75421
+    -- print $ sortN 329450 == 954320
+    -- print $ sortN 9125 == 9521
+
+countDigits :: Int -> Int
+countDigits 0 = 0
+countDigits number = 1 + countDigits (div number 10)
+
+findMax :: Int -> Int
+findMax number = helper (div number 10) (mod number 10)
+ where
+    helper :: Int -> Int -> Int
+    helper 0 result = result
+    helper leftover result
+     | mod leftover 10 > result = helper (div leftover 10) (mod leftover 10)
+     | otherwise = helper (div leftover 10) result
+
+sortN :: Int -> Int
+sortN number = helper number 0 0 0
+ where
+    helper :: Int -> Int -> Int -> Int -> Int
+    helper number maxDigit count result
+     |
+    maxDigit = findMax number
+    count = countDigits number 
