037d7439b6d6d2a83663baa0d57de7da2f2948b4
elenakarabeteva <e.karabeteva6@gmail.com>
2023-05-14 14:51:53 +0300

    completed one functionality of third task

---
+++ Week06/ForHome/Task03.hs
@@ -0,0 +1,27 @@
+main :: IO()
+main = do
+    -- print $ sumRats (2, 5) (5, 2) -- == (29, 10)
+    -- print $ sumRats (52, 123) (96, 14) -- == (6268, 861)
+    -- print $ sumRats (2, 5) (3, 5) -- == (1, 1)
+
+    print $ multiplyRats (2, 5) (5, 2) == (1, 1)
+    print $ multiplyRats (52, 123) (96, 14) == (832, 287)
+    print $ multiplyRats (2, 5) (3, 5) == (6, 25)
+
+    -- print $ divideRats (2, 5) (5, 2) -- == (4, 25)
+    -- print $ divideRats (52, 123) (96, 14) -- == (91, 1476)
+    -- print $ divideRats (2, 5) (3, 5) -- == (2, 3)
+
+    -- print $ areEqual (2, 5) (5, 2) == False
+    -- print $ areEqual (52, 123) (52 * 3, 123 * 3) == True
+    -- print $ areEqual (2, 6) (5, 15) == True
+
+type Rat a = (a, a)
+
+normalize :: (Integral a) => Rat a -> Rat a
+normalize (x, y) = (div x d, div y d)
+ where
+    d = gcd x y
+
+multiplyRats :: (Integral a) => Rat a -> Rat a -> Rat a
+multiplyRats first second = normalize (fst first * fst second, snd first * snd second)
