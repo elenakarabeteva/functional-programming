973f03ca2594ba84a3f87288d5a22a9f5ab08761
elenakarabeteva <e.karabeteva6@gmail.com>
2023-05-18 19:55:06 +0300

    completed sumRats function

--- Week06/ForHome/Task03.hs
+++ Week06/ForHome/Task03.hs
@@ -1,8 +1,8 @@
 main :: IO()
 main = do
-    -- print $ sumRats (2, 5) (5, 2) -- == (29, 10)
-    -- print $ sumRats (52, 123) (96, 14) -- == (6268, 861)
-    -- print $ sumRats (2, 5) (3, 5) -- == (1, 1)
+    print $ sumRats (2, 5) (5, 2) == (29, 10)
+    print $ sumRats (52, 123) (96, 14) == (6268, 861)
+    print $ sumRats (2, 5) (3, 5) == (1, 1)
 
     print $ multiplyRats (2, 5) (5, 2) == (1, 1)
     print $ multiplyRats (52, 123) (96, 14) == (832, 287)
@@ -25,3 +25,6 @@ normalize (x, y) = (div x d, div y d)
 
 multiplyRats :: (Integral a) => Rat a -> Rat a -> Rat a
 multiplyRats first second = normalize (fst first * fst second, snd first * snd second)
+
+sumRats :: (Integral a) => Rat a -> Rat a -> Rat a
+sumRats (x1, y1) (x2, y2) = normalize (x1 * y2 + x2 * y1, y1 * y2)
