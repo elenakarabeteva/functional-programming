6326f4e4037250bb82faf10e0cd72a5498f3f362
elenakarabeteva <e.karabeteva6@gmail.com>
2023-05-31 00:29:56 +0300

    added code of distance method between two points

--- Additional/Week09.hs
+++ Additional/Week09.hs
@@ -3,7 +3,14 @@ import Data.List
 main :: IO()
 main = do
     print (addMatrices [[1,2],[3,4]] [[4,3],[2,1]])
+    print $ distance (Point2D 1 1) (Point2D 2 2)
 
 addMatrices :: (Num a, Eq a) => [[a]] -> [[a]] -> [[a]]
 addMatrices [] [] = []
 addMatrices (m:ms) (n:ns) = zipWith (+) m n : addMatrices ms ns
+
+data Point2D = Point2D Double Double
+ deriving (Read, Show)
+
+distance :: Point2D -> Point2D -> Double
+distance (Point2D x1 y1) (Point2D x2 y2) = sqrt ((x1 - x2)^2 + (y1 - y2)^2)
