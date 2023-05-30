ac5939c7a1708dc53e9b34a2e8881683072fcd32
elenakarabeteva <e.karabeteva6@gmail.com>
2023-05-30 17:19:22 +0300

    rewrited line of code

--- Week06/ForHome/Task06.hs
+++ Week06/ForHome/Task06.hs
@@ -3,4 +3,4 @@ main = do
     print $ (getOddCompositionValue [(\x -> x + 1),(\x -> x * 2),(\x -> x - 1), (\x -> div x 2)]) 2 == 2
 
 getOddCompositionValue :: [Int -> Int] -> Int -> Int
-getOddCompositionValue = foldr1 (.) . map snd . filter (odd . fst) . zip [0..]
+getOddCompositionValue = foldr1 (.) . map snd . filter (odd . fst) . zip [1..]
