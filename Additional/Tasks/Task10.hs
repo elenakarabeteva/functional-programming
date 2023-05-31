b10c2764d05fcc600f3fa2f8bd1223d14232faef
elenakarabeteva <e.karabeteva6@gmail.com>
2023-06-01 01:13:32 +0300

    task ten completed

---
+++ Additional/Tasks/Task10.hs
@@ -0,0 +1,6 @@
+main :: IO()
+main = do
+    print $ (myPoly [2.7, 3.0 ..]) 2.2 3 == -0.4399999999999998
+
+myPoly :: (Num a) => [a] -> (a -> Int -> a)
+myPoly xs = (\ x y -> product $ map (x-) $ take y xs)
